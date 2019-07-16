9fe814a91ed0c8f962fae49539f468c82af53bf1
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2019-07-15 17:08:29 -0700

    make Time interface into a class so that it can have validation methods

--- src/@types/classclockTypes.ts
+++ src/@types/classclockTypes.ts
@@ -18,8 +18,32 @@ export interface Period {
     endTime: Time;
 }
 
-export interface Time {
-    hours: number;
-    minutes: number;
-    seconds?: number;
+export class Time {
+    private hours: number;
+    private minutes: number;
+    private seconds?: number;
+
+    constructor(hours: number, minutes: number, seconds?: number) {
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
+    }
+
+    public getHours() {
+        return this.hours;
+    }
+    public getMinutes() {
+        return this.minutes;
+    }
+    public getSeconds() {
+        return this.seconds;
+    }
+
+    public get_valid_time(): Time {
+        return new Time(
+            this.hours % 24,
+            this.minutes % 60,
+            this.seconds !== undefined ? this.seconds % 60 : undefined
+        );
+    }
 }
