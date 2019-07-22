8240fb90acacaedca82e2261680e89b309df54db
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2019-07-21 21:13:55 -0700

    finish initial classclock service class

--- src/services/classclock.ts
+++ src/services/classclock.ts
@@ -1,3 +1,26 @@
-export const ClassClockService = () => {
-    const baseURL = "http://localhost:5000/v0";
-};
+export default class ClassClockService {
+    public baseURL: string = "http://localhost:5000/v0";
+
+    getSchoolsList = async (authToken: string): Promise<Response> => {
+        return await fetch(this.baseURL + "/schools/", this.getHeaders(authToken, "GET"));
+    };
+
+    getSchool = async (authToken: string, schoolId: string): Promise<Response> => {
+        return await fetch(
+            this.baseURL + "/school/" + schoolId + "/",
+            this.getHeaders(authToken, "GET")
+        );
+    };
+    private getHeaders = (
+        authToken: string,
+        method: string
+    ): { method: string; headers: Headers } => {
+        return {
+            method,
+            headers: new Headers({
+                Accept: "application/vnd.api+json",
+                Authorization: "Bearer " + authToken
+            })
+        };
+    };
+}
