4379e95e8075a972481bb7cf2b890055d6b6148d
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2019-07-25 21:55:17 -0700

    change Schedule container from a class into a function component

--- src/pages/Schedule.tsx
+++ src/pages/Schedule.tsx
@@ -4,27 +4,46 @@ import { push } from "redux-first-routing";
 import IPageInterface from "../utils/IPageInterface";
 
 import "../global.css";
+import School from "../@types/school";
+import { IState } from "../store/schools/types";
+import Link from "../components/Link";
+import { pages } from "../utils/constants";
+import Icon from "../components/Icon";
 
-class Schedule extends Component<IPageInterface, {}> {
+export interface IAppProps {
+    selectedSchool: {
+        isFetching: boolean;
+        didInvalidate: false;
+        data: School;
+    };
+    dispatch: any;
+}
+
+const Schedule = (props: IAppProps) => {
     // document.getElementById("schoolName").innerHTML = schools[selectedSchoolIndex].fullName;
     // document.getElementById("scheduleDisplay").innerHTML = getCurrentScheduleName() + " schedule.";
 
     // populateScheduleTable();
 
     //input:  onchange="updateSettings()"
-    render() {
-        return (
-            <div>
-                <a className="navbutton" href="index.html">
-                    <i className="fas fa-home" />
-                </a>
-                <br />
-                <h1 className="centered topSpace bottomSpace" id="schoolName" />
-                <p className="centered bottomSpace" id="scheduleDisplay" />
-                <table id="scheduleTable" className="centeredInline topSpace" />
-            </div>
-        );
-    }
-}
 
-export default connect()(Schedule);
+    return (
+        <div>
+            <a className="navbutton" href="index.html">
+                <i className="fas fa-home" />
+            </a>
+            <br />
+            <h1 className="centered topSpace bottomSpace" id="schoolName" />
+            <p className="centered bottomSpace" id="scheduleDisplay" />
+            <table id="scheduleTable" className="centeredInline topSpace" />
+        </div>
+    );
+};
+
+const mapStateToProps = (state: IState) => {
+    const { selectedSchool } = state;
+    selectedSchool.data = School.fromJson(selectedSchool.data);
+    return { selectedSchool };
+};
+
+export default connect(mapStateToProps)(Schedule);
