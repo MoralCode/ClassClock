3f03095b1bb06ffc327ff68bcfa26c1a8237e9cf
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2019-07-25 18:47:28 -0700

    move text compnent CSS to its own file

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,4 +1,5 @@
 import React, { Component, CSSProperties } from "react";
+import "./Text.css";
 
 interface ITextProps {
     className?: string;
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -4,7 +4,7 @@ import { push } from "redux-first-routing";
 import "../global.css";
 import Link from "../components/Link";
 import Icon from "../components/Icon";
-import Text from "../components/Text";
+import Text from "../components/Text/Text";
 import Time from "../@types/time";
 import School from "../@types/school";
 import { pages } from "../utils/constants";
