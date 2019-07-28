b89cbcdf0dae76dfd8342666093139b692fe184c
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2019-07-27 18:16:38 -0700

    add classPeriod tests

---
+++ src/@types/classperiod.test.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import BellSchedule from "./bellschedule";
+import ClassPeriod from "./classperiod";
+import Time from "./time";
+
+const currentDate = new Date();
+
+const name = "First Period";
+
+const startTime = new Time(8, 25);
+const endTime = new Time(9, 55);
+
+const classPeriodA = new ClassPeriod(name, startTime, endTime, currentDate);
+describe("ClassPeriod", () => {
+    it("gets from json", () => {
+        expect(
+            ClassPeriod.fromJson({
+                name: "First Period",
+                startTime: "08:25",
+                endTime: "09:55",
+                currentDate
+            })
+        ).toBe(classPeriodA);
+    });
+
+    //gonna assume the constructor works
+
+    it("can get its name", () => {
+        expect(classPeriodA.getName()).toBe(name);
+    });
+
+    it("can get its start time", () => {
+        expect(classPeriodA.getStartTime()).toBe(startTime);
+    });
+
+    it("can get its end time", () => {
+        expect(classPeriodA.getEndTime()).toBe(endTime);
+    });
+
+    it("can get its duration", () => {
+        expect(classPeriodA.getDuration()).toBe(new Time(1, 30));
+    });
+
+    it("can get its duration", () => {
+        expect(classPeriodA.getDuration()).toBe(new Time(1, 30));
+    });
+});
