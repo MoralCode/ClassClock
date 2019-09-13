dfc3e5fcfffd6f15b099ec906684555faeaca8cc
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2019-09-13 11:26:54 -0400

    add onError handlers for api calls

--- src/store/schools/actions.ts
+++ src/store/schools/actions.ts
@@ -43,38 +43,57 @@ export function selectSchool(schoolId: string) {
     return async (dispatch: Dispatch) => {
         dispatch(requestSchool());
 
+        const onError = (error: Error) => {
+            console.log("Caught an error: ", error.message);
+            dispatch(fetchError(error.message));
+        };
+
         const school = ClassClockService.validateResponse(
-            ClassClockService.getSchool(schoolId)
+            ClassClockService.getSchool(schoolId),
+            onError
         );
 
         const schedules = ClassClockService.validateResponse(
-            ClassClockService.getSchedulesListForSchool(schoolId)
+            ClassClockService.getSchedulesListForSchool(schoolId),
+            onError
         );
 
-        Promise.all([school, schedules]).then((result: any) => {
-            const [schoolResult, scheduleResult] = result;
+        Promise.all([school, schedules]).then(
+            (result: any) => {
+                const [schoolResult, scheduleResult] = result;
 
-            //result = [school() result, schedules() result]
-            //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/all#Using_Promise.all
+                //result = [school() result, schedules() result]
+                //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/all#Using_Promise.all
 
-            const scheduleDataList: Array<Promise<any>> = [];
+                const scheduleDataList: Array<Promise<any>> = [];
 
-            for (const schedule of scheduleResult.data) {
-                const scheduleId = schedule.id;
-                // const sched_uri = schedule.links.self;
-                const scheduleRequest = ClassClockService.validateResponse(
-                    ClassClockService.getDetailedScheduleForSchool(schoolId, scheduleId)
+                for (const schedule of scheduleResult.data) {
+                    const scheduleId = schedule.id;
+                    // const sched_uri = schedule.links.self;
+                    const scheduleRequest = ClassClockService.validateResponse(
+                        ClassClockService.getDetailedScheduleForSchool(
+                            schoolId,
+                            scheduleId
+                        ),
+                        onError
+                    );
+
+                    scheduleDataList.push(scheduleRequest);
+                }
+
+                Promise.all(scheduleDataList).then(
+                    (schedulesList: any) => {
+                        schoolResult.data.attributes.schedules = schedulesList.map(
+                            (schedule: any) => deconstructJsonApiResource(schedule.data)
+                        );
+                        dispatch(
+                            receiveSchool(deconstructJsonApiResource(schoolResult.data))
+                        );
+                    },
+                    (error: Error) => onError(error)
                 );
-
-                scheduleDataList.push(scheduleRequest);
-            }
-
-            Promise.all(scheduleDataList).then((schedulesList: any) => {
-                schoolResult.data.attributes.schedules = schedulesList.map(
-                    (schedule: any) => deconstructJsonApiResource(schedule.data)
-                );
-                dispatch(receiveSchool(deconstructJsonApiResource(schoolResult.data)));
-            });
-        });
+            },
+            (error: Error) => onError(error)
+        );
     };
 }
