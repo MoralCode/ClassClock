290b1c91bda30bfb968b30aa23bffd38da9c7f89
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2019-12-16 15:54:19 -0500

    add tests for checkTimeRange() and getNextImportantTime()

--- src/utils/helpers.test.ts
+++ src/utils/helpers.test.ts
@@ -3,12 +3,14 @@ import {
     getValueIfKeyInList,
     deconstructJsonApiResource,
     sortClassesByStartTime,
-    getTimeStateForDateAtSchool
+    getTimeStateForDateAtSchool,
+    getNextImportantTime,
+    checkTimeRange
 } from "./helpers";
 import ClassPeriod from "../@types/classperiod";
 import Time from "../@types/time";
-import { classPeriod2, classPeriod, beforeSchoolHours, school, betweenClass, inClass, noSchool, afterSchoolHours, bellScheduleClasses } from "./testconstants";
-import { TimeStates } from "./enums";
+import { classPeriod2, classPeriod, beforeSchoolHours, school, betweenClass, inClass, noSchool, afterSchoolHours, bellScheduleClasses, duringClass, startTime, endTime, startTime2, beforeClass, endTime2 } from "./testconstants";
+import { TimeStates, TimeComparisons } from "./enums";
 
 test("get value if key in list", () => {
     const object1 = { value1: "foo" };
@@ -76,4 +78,49 @@ test("get time states for given date and school", () => {
         TimeStates.OUTSIDE_SCHOOL_HOURS
     );
     
-})
+});
+
+
+test("get next important time", () => {
+    expect(getNextImportantTime(beforeSchoolHours, school)).toBe([
+        classPeriod,
+        classPeriod.getStartTime()
+    ]);
+
+    expect(getTimeStateForDateAtSchool(noSchool, school)).toBeFalsy();
+    
+    expect(getTimeStateForDateAtSchool(betweenClass, school)).toBe([
+        classPeriod2,
+        classPeriod2.getStartTime()
+    ]);
+
+    expect(getTimeStateForDateAtSchool(inClass, school)).toBe([
+        classPeriod,
+        classPeriod.getEndTime()
+    ]);
+
+    expect(getTimeStateForDateAtSchool(afterSchoolHours, school)).toBeFalsy();
+});
+
+test("check time range", () => {
+
+    expect(checkTimeRange(duringClass, startTime, endTime)).toBe(
+        TimeComparisons.IS_DURING_OR_EXACTLY
+    );
+
+    expect(checkTimeRange(duringClass, endTime, startTime2)).toBe(
+        TimeComparisons.IS_BEFORE
+    );
+
+    expect(checkTimeRange(beforeClass, startTime, endTime2)).toBe(
+        TimeComparisons.IS_BEFORE
+    );
+
+    expect(checkTimeRange(startTime, startTime, endTime)).toBe(
+        TimeComparisons.IS_DURING_OR_EXACTLY
+    );
+
+    expect(checkTimeRange(endTime, startTime, endTime)).toBe(
+        TimeComparisons.IS_DURING_OR_EXACTLY
+    );
+});
