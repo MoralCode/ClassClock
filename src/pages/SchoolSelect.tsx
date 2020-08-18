3ce937004544fb98cbfe831f1e4307e59a3c0281
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2020-08-17 21:48:27 -0700

    update SchoolSelect imports

--- src/pages/SchoolSelect.tsx
+++ src/pages/SchoolSelect.tsx
@@ -1,16 +1,13 @@
 import React, { Component, useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { push } from "redux-first-routing";
-import "../../global.css";
-import "./SchoolSelect.css";
-
-import { selectSchool } from "../../store/schools/actions";
-import { useAuth0 } from "../../react-auth0-wrapper";
-import School from "../../@types/school";
-import ClassClockService from "../../services/classclock";
-import { pages } from "../../utils/constants";
-import { IState } from "../../store/schools/types";
-import SelectionList from "../../components/SelectionList/SelectionList";
+import "../global.css";
+import { selectSchool } from "../store/schools/actions";
+import School from "../@types/school";
+import ClassClockService from "../services/classclock";
+import { pages } from "../utils/constants";
+import { IState } from "../store/schools/types";
+import SelectionList from "../components/SelectionList/SelectionList";
 
 export interface ISelectProps {
     selectedSchool: any;
