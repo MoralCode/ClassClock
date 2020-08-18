c27ed739c8188a920c79e08ca8837c04cbc30adb
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2020-08-17 21:36:34 -0700

    remove unused imports

--- src/components/SelectionList/SelectionList.tsx
+++ src/components/SelectionList/SelectionList.tsx
@@ -1,15 +1,5 @@
-import React, { Component, useEffect, useState } from "react";
-import { connect } from "react-redux";
-import { push } from "redux-first-routing";
 import "../../global.css";
 import "./SelectionList.css";
-
-import { selectSchool } from "../../store/schools/actions";
-import { useAuth0 } from "../../react-auth0-wrapper";
-import School from "../../@types/school";
-import ClassClockService from "../../services/classclock";
-import { pages } from "../../utils/constants";
-import { IState } from "../../store/schools/types";
 
 export interface ISelectProps {
     title: string;
