1ff90ab04c05b95af92c9d7afe5f6ba6b1706a45
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2020-08-18 12:58:52 -0700

    move EditableField component to a new file

---
+++ src/components/EditableField.tsx
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+
+
+interface IEditableFieldProps {
+	label: React.ReactNode;
+	value: string | number | string[] | undefined;
+	onChange: () => any;
+	disabled?: boolean | undefined;
+	readOnly?: boolean | undefined;
+	placeholder?: string | undefined;
+}
+
+export default class EditableField extends Component<IEditableFieldProps, {}> {
+	render() {
+		return (<label style={{
+			display: "inline-flex",
+			justifyContent: "space-between",
+			width: "100%",
+			textAlign: "left",
+			margin: "2px"
+		}}>
+			<span>{this.props.label}</span>
+			<input
+				type="text"
+				value={this.props.value}
+				disabled={this.props.disabled}
+				readOnly={this.props.readOnly}
+				onChange={this.props.onChange}
+				placeholder={this.props.placeholder}
+			/>
+		</label>
+		)
+	}
+}
