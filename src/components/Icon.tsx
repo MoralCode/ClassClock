f06fa3844a3b227af100c5d24642aee292de6716
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2020-09-13 00:10:16 -0700

    add regularStyle flag to Icon component to allow style customization

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -2,10 +2,12 @@ import React, { Component } from "react";
 
 interface IIconProps {
     icon: string;
+    regularStyle?:boolean
 }
 
 const Icon = (props: IIconProps) => {
-    return <i className={"fas " + props.icon} />;
+    const style = props.regularStyle? "far ": "fas ";
+    return <i className={style + props.icon} />;
 }
 
 export default Icon;
