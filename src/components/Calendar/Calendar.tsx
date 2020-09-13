MoralCode/ClassClock

commit 10bcaf74e9c1cde80febbcd7c5f8c22b6537dbed

refactor setScheduleForDate to accept a schedule as a parameter

src/components/Calendar/Calendar.tsx
@@ -77,40 +77,35 @@ const Calendar = (props: ICalendarProps) => {
     //     }
     // };
 
-    const setScheduleForDate = (date: Date, scheduleId?: string) => {
-        const location = getScheduleAndIndexForDate(date);
+    const setScheduleForDate = (date: Date, schedule?: BellSchedule) => {
+        const currentScheduleandIndex = getScheduleAndIndexForDate(date);
+        const currentSchedule = currentScheduleandIndex ? currentScheduleandIndex[0] : undefined;
 
-        if (location && scheduleId) {
-            const [optionKey, posInOption] = location;
+
+        if (currentSchedule && schedule) {
             //the date is in a different schedule than the one provided. move it
-            if (optionKey !== scheduleId) {
-                let updatedSelections = props.options;
+            if (currentSchedule.getIdentifier() !== schedule.getIdentifier()) {
                 //remove from the old schedule
-                updatedSelections = removeDateFromSelectionList(
-                    updatedSelections,
-                    optionKey,
-                    posInOption
-                );
+                currentSchedule.removeDate(date);
+                
                 //add to the new schedule
-                updatedSelections = addDateToSelectionList(
-                    updatedSelections,
-                    scheduleId,
-                    date
-                );
-                props.onDateChange(updatedSelections);
+                schedule.addDate(date);
+
+                //callback
+                props.onDateChange(date, currentSchedule, schedule);
             
             } else {
-                props.onDateChange(
-                    removeDateFromSelectionList(props.options, optionKey, posInOption)
-                );
+                //theyre in the same schedule, no change
+                // props.onDateChange(date, schedule, schedule);
             }
-        } else if (!location && scheduleId) {
-            props.onDateChange(addDateToSelectionList(props.options, scheduleId, date));
-        } else if (location && !scheduleId) {
-            const [optionKey, posInOption] = location;
-            props.onDateChange(
-                removeDateFromSelectionList(props.options, optionKey, posInOption)
-            );
+        } else if (!currentSchedule && schedule) {
+            //date was not in a schedule previously
+            schedule.addDate(date)
+            props.onDateChange(date, undefined, schedule);
+        } else if (currentSchedule && !schedule) {
+            //date is in a schedule and is being removed
+            currentSchedule.removeDate(date)
+            props.onDateChange(date, currentSchedule, undefined);
         }
     };
 

commit decbe969f18626fac7bce1e8cd24e5328935629e

fix bug introduced in 10bcaf74e9c1cde80febbcd7c5f8c22b6537dbed where clicking on an existing date would not remove it from the schedule
This also moves this behavior outside of setScheduleForDate

src/components/Calendar/Calendar.tsx
@@ -35,12 +35,17 @@ const Calendar = (props: ICalendarProps) => {
     const onDateClick = (event: React.MouseEvent<HTMLElement, MouseEvent>) => {
         const dateValue: Date = new Date(parseInt(event.currentTarget.dataset.date!, 10));
         if (isValidDate(dateValue)) {
+            const currentSchedule = getScheduleForDate(dateValue);
             const selectedSchedule = getScheduleById(props.selectedScheduleId);
+            let value = selectedSchedule;
+
             if (!selectedSchedule) {
                 alert("Please select a schedule to assign a date")
-            } else {
-                setScheduleForDate(dateValue, selectedSchedule);
+            } else if (currentSchedule && (selectedSchedule.getIdentifier() === currentSchedule.getIdentifier())) {
+                //if the date belongs to this schedule, remove it
+                value = undefined;
             }
+            setScheduleForDate(dateValue, value);
         } else {
             console.log("invalid date");
         }
@@ -63,15 +68,12 @@ const Calendar = (props: ICalendarProps) => {
 
         if (currentSchedule && schedule) {
             //the date is in a different schedule than the one provided. move it
-            if (currentSchedule.getIdentifier() !== schedule.getIdentifier()) {
-                //remove from the old schedule
-                currentSchedule.removeDate(date);
-                
-                //add to the new schedule
-                schedule.addDate(date);
-            } else {
-                //theyre in the same schedule, no change
-            }
+            //remove from the old schedule
+            currentSchedule.removeDate(date);
+            
+            //add to the new schedule
+            schedule.addDate(date);
+            
         } else if (!currentSchedule && schedule) {
             //date was not in a schedule previously
             schedule.addDate(date)