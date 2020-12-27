3d21ff478c40077c87535970942f76af062bc43c
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2020-12-27 14:36:27 -0800

    update current time display to obey user preferences

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -122,7 +122,7 @@ export const App = (props: IAppProps) => {
             <Block>
                 <p>It is currently: </p>
                 <p className="timeFont" style={{ fontSize: "40px" }}>
-                    {currentDate.toLocaleString(DateTime.TIME_SIMPLE)}
+                    {props.userSettings.use24HourTime ? currentDate.toLocaleString({ hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false }) : currentDate.toLocaleString({ hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: true }) }
                 </p>
                 <p>
                     on{" "}
