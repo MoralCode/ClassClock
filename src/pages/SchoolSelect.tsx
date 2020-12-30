f7d45fb6b1d6caee9e03a962048e234f8e284818
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2020-12-30 15:00:30 -0800

    update SelectionList to use schoolList from redux.

--- src/pages/SchoolSelect.tsx
+++ src/pages/SchoolSelect.tsx
@@ -1,48 +1,35 @@
-import React, { Component, useEffect, useState } from "react";
+import React, { Component, useEffect } from "react";
 import { connect } from "react-redux";
 import { push } from "redux-first-routing";
 import "../global.css";
-import { selectSchool } from "../store/schools/actions";
+import { getSchoolsList, selectSchool } from "../store/schools/actions";
 import School from "../@types/school";
-import ClassClockService from "../services/classclock";
 import { pages } from "../utils/constants";
-import { ISchoolsState, SelectedSchoolState } from "../store/schools/types";
+import { ISchoolListState, SchoolListState } from "../store/schools/types";
 import SelectionList from "../components/SelectionList/SelectionList";
+import { DateTime } from "luxon";
 
 export interface ISelectProps {
-    selectedSchool: SelectedSchoolState;
+    schoolList: SchoolListState;
     error: string;
     dispatch: any;
 }
 
 const SchoolSelect = (props: ISelectProps) => {
-    const [schoolList, setSchoolList] = useState([]);
-    const [lastRefresh, setlastRefresh] = useState(0);
 
     useEffect(() => {
         const controller = new AbortController();
         const signal = controller.signal;
 
-        if (
-            isFetching() &&
-            Math.abs(new Date().getTime() - lastRefresh) > 120000 //120000 ms = 2 min
-        ) {
-            const fetchSchools = async (abortSignal: AbortSignal) => {
-                ClassClockService.validateResponse(
-                    ClassClockService.getSchoolsList({
-                        signal: abortSignal
-                    })
-                ).then((json: any) => {
-                    setSchoolList(
-                        json.data.map((value: any) =>
-                            School.fromJson(value)
-                        )
-                    );
+        const lastUpdate = props.schoolList.lastUpdated || 0
 
-                    setlastRefresh(new Date().getTime());
-                });
-            };
-            fetchSchools(signal);
+        if (
+            
+            DateTime.local().toMillis() - lastUpdate > 120000 //120000 ms = 2 min
+        ) {
+            
+            props.dispatch(getSchoolsList(signal))
+
         }
 
         return () => {
@@ -52,10 +39,10 @@ const SchoolSelect = (props: ISelectProps) => {
 
 
      //schoolList.length is used here because the app can potentially get stuck on isFetching = true if, for example, the page gets closed while a request is in progress. maybe mitigate this with a timestamp of when the requet started and add a timeout to change it back to false automatically?
-    const isFetching = () => schoolList.length === 0
+    const isFetching = () => props.schoolList.isFetching
     // props.selectedSchool.isFetching === false
 
-    const list = schoolList.map((school: School) => (
+    const list = props.schoolList.data.map((school: School) => (
         <li
             key={school.getIdentifier()}
             onClick={() => {
@@ -76,8 +63,8 @@ const SchoolSelect = (props: ISelectProps) => {
     );
 };
 
-const mapStateToProps = (state: ISchoolsState & { error: string }) => {
-    const { selectedSchool, error } = state;
-    return { selectedSchool, error };
+const mapStateToProps = (state: ISchoolListState & { error: string }) => {
+    const { schoolList, error } = state;
+    return { schoolList, error };
 };
 export default connect(mapStateToProps)(SchoolSelect);
