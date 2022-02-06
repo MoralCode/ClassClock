5c1c9c3737ebdc698b7dd17a402f123e2687b915
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2022-02-06 17:07:21 -0500

    update and cleanup authProvider parameters

--- src/pages/Admin/authProvider.ts
+++ src/pages/Admin/authProvider.ts
@@ -1,22 +1,14 @@
 
-import { Auth0Client } from '@auth0/auth0-spa-js';
+import { LogoutOptions, RedirectLoginOptions } from '@auth0/auth0-spa-js';
+import { AuthProvider } from 'react-admin';
 import {Auth0, pages} from '../../utils/constants'
 
-const auth0 = new Auth0Client({
-	domain: Auth0.domain,
-	client_id: Auth0.clientID,
-	redirect_uri: process.env.REACT_APP_AUTH0_REDIRECT_URI,
-	cacheLocation: 'localstorage',
-	useRefreshTokens: true
-});
-
-const authProvider = ({
-  isAuthenticated,
-  loginWithRedirect,
-  loading,
-  logout,
-  user,
-}) => ({
+const authProvider = (
+	isAuthenticated: boolean,
+	loading: boolean,
+	logout: (o?: LogoutOptions) => void,
+	user:any,
+) => ({
 	login: () => {
 		console.log("login")
 		return (isAuthenticated && loading == false ? Promise.resolve() : Promise.reject())
