af78286b47c2e65e8276b5f38a0023bca1aa3719
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2022-02-06 10:16:50 -0500

    remove x-total-count check/error messge

--- src/services/classclock-dataprovider.ts
+++ src/services/classclock-dataprovider.ts
@@ -48,11 +48,11 @@ export default (apiUrl, httpClient = fetchUtils.fetchJson): DataProvider => ({
 		const url = `${apiUrl}/${resource}`;
 
 		return httpClient(url).then(({ headers, json }) => {
-			if (!headers.has('X-Total-Count')) {
-				throw new Error(
-					'The X-Total-Count header is missing in the HTTP Response. The jsonServer Data Provider expects responses for lists of resources to contain this header with the total number of results to build the pagination. If you are using CORS, did you declare X-Total-Count in the Access-Control-Expose-Headers header?'
-				);
-			}
+			// if (!headers.has('X-Total-Count')) {
+			// 	throw new Error(
+			// 		'The X-Total-Count header is missing in the HTTP Response. The jsonServer Data Provider expects responses for lists of resources to contain this header with the total number of results to build the pagination. If you are using CORS, did you declare X-Total-Count in the Access-Control-Expose-Headers header?'
+			// 	);
+			// }
 			return {
 				data: json,
 				total: parseInt(
