82f25fb5a1181cb804fe1b3d915483c4ff5b11a1
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2022-02-07 22:18:24 -0500

    remove debug logging statements from authProvider

--- src/pages/Admin/authProvider.ts
+++ src/pages/Admin/authProvider.ts
@@ -10,12 +10,10 @@ const authProvider = (
 	user:any,
 ) => ({
 	login: () => {
-		console.log("login")
 		return (isAuthenticated && loading == false ? Promise.resolve() : Promise.reject())
 	},
 	// TODO: maybe use login: () => Promise.resolve(),//unused because login is handled by auth0
 	logout: () => {
-		console.log("logout")
 		logout(
 			// TODO: is this needed?
 			//federated: true // have to be enabled to invalidate refresh token
@@ -23,19 +21,16 @@ const authProvider = (
 		return (isAuthenticated && loading == false ? Promise.resolve() : Promise.reject())
 	},
 	checkError: ({ status }: {status:number}) => {
-		console.log("checkError")
 		if (status === 401 || status === 403) {
 			return Promise.reject();
 		}
 		return Promise.resolve();
 	},
 	checkAuth: () => {
-		console.log("checkAuth: " + isAuthenticated)
 		return ((isAuthenticated || loading) ? Promise.resolve() : Promise.reject())
 		// TODO: is this a good idea/necessary? return Promise.reject({ redirectTo: '/nologin' })
 	},
 	getPermissions: () => {
-		console.log("getPermissions")
 		return ((isAuthenticated || loading) ? Promise.resolve() : Promise.reject())
 	},
 	getIdentity: () =>
