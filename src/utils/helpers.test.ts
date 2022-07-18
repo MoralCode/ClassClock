c84d6b87dc7e68137371a97e83e5afa3b9d7b005
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2022-07-17 23:09:42 -0400

    days are no longer involved in the checkTimeRange process

--- src/utils/helpers.test.ts
+++ src/utils/helpers.test.ts
@@ -121,36 +121,3 @@ test("check time range with start and end swapped", () => {
         TimeComparisons.IS_DURING_OR_EXACTLY
     );
 });
-
-test("check time range ignoring day", () => {
-
-    expect(checkTimeRange(duringClass.plus({ days: 1 }), startTime, endTime, true)).toBe(
-        TimeComparisons.IS_DURING_OR_EXACTLY
-    );
-
-    expect(checkTimeRange(duringClass.plus({ days: 1 }), endTime, startTime2, true)).toBe(
-        TimeComparisons.IS_BEFORE
-    );
-
-    expect(checkTimeRange(beforeClass.plus({ days: 1 }), startTime, endTime2, true)).toBe(
-        TimeComparisons.IS_BEFORE
-    );
-
-    expect(checkTimeRange(startTime.plus({ days: 1 }), startTime, endTime, true)).toBe(
-        TimeComparisons.IS_DURING_OR_EXACTLY
-    );
-
-    expect(checkTimeRange(endTime.plus({ days: 1 }), startTime, endTime, true)).toBe(
-        TimeComparisons.IS_DURING_OR_EXACTLY
-    );
-
-    expect(checkTimeRange(afterClass.plus({ days: 1 }), startTime, endTime, true)).toBe(
-        TimeComparisons.IS_AFTER
-    );
-
-    expect(checkTimeRange(afterClass.plus({ days: 1 }), startTime, endTime2, true)).toBe(
-        TimeComparisons.IS_DURING_OR_EXACTLY
-    );
-
-
-});
