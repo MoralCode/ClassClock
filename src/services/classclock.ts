bfa36ff6a5f78893895159d92fe5d21620841655
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2022-08-11 18:38:37 -0400

    remove old use of fate-fns

--- src/services/classclock.ts
+++ src/services/classclock.ts
@@ -1,5 +1,4 @@
 import BellSchedule from "../@types/bellschedule";
-import { format } from 'date-fns'
 import { delay, objectKeysToSnakeCase, parseRateLimitTime, promiseRetry } from "../utils/helpers";
 import { DateTime } from "luxon";
 import { RateLimitError } from "../utils/errors";
@@ -114,15 +113,11 @@ export default class ClassClockService {
     private static jsonifyReplacer(key: string, value: any) {
         console.log(key, value)
         if (key == 'date') {
-            return format(value, 'YYYY-MM-DD');
-            // Date-fns v2
-            // return format(value, 'yyyy-MM-dd');
+            return value.toFormat('YYYY-MM-DD');
         } else if (key == 'dates') {
 
             return value.map((currentValue: DateTime) => {
                 return currentValue.toFormat('YYYY-MM-DD');
-                // Date-fns v2
-                // return format(value, 'yyyy-MM-dd');
             })
         } else if (Object.prototype.toString.call(value) === '[object Array]') {
 
