40adbf9ab088c38556cd6e07a2663279bbc18537
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2022-08-11 19:10:17 -0400

    reuse social icons on the error pages

--- src/pages/errors/PageNotFound.tsx
+++ src/pages/errors/PageNotFound.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import SocialIcons from "../../components/SocialIcons";
 
 export const PageNotFound = () => {
     return (
         <>
         <h1>Page Not Found!</h1>
-        <p>Why not try another page?</p>
+        <p>Why not try another page or reach out to us</p>
+        <SocialIcons />
         </>
     );
 };
--- src/pages/errors/ServerError.tsx
+++ src/pages/errors/ServerError.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import SocialIcons from "../../components/SocialIcons";
 
 export const ServerError = () => {
     return (
         <>
         <h1>Oops! Something went wrong</h1>
-        <p>Why not try another page?</p>
+        <p>Please reach out if this happens repeatedly. Your detailed feedback helps get things fixed faster</p>
+        <SocialIcons />
         </>
     );
 };
