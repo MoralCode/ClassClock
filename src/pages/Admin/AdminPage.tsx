0a58e9cfb976fe6eaa0a46844e0c04f3a0df0a09
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2022-11-24 11:01:49 -0500

    add some content to the admin homepage so its less bare and 404-y

--- src/pages/Admin/AdminPage.tsx
+++ src/pages/Admin/AdminPage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Admin as RAdmin, Resource, ListGuesser, EditGuesser, EditButton, DeleteButton} from "react-admin";
+import { Admin as RAdmin, Resource, ListGuesser, EditGuesser, EditButton, DeleteButton, CustomRoutes} from "react-admin";
 import ccDataProvider from "../../services/classclock-dataprovider"
 import authProvider from "../../pages/Admin/authProvider";
 import { useAuth0 } from "@auth0/auth0-react";
@@ -7,6 +7,10 @@ import ClassClockService from "../../services/classclock";
 import { BrowserRouter } from 'react-router-dom';
 import LoginRedirect from "./LoginRedirect";
 import { BellScheduleCreate, BellscheduleEdit, BellScheduleList } from "./resources";
+import { Route } from "react-router-dom";
+
+import { Card, CardContent } from '@mui/material';
+import { Title } from 'react-admin';
 
 
 const AdminPage = () => {
@@ -27,6 +31,18 @@ const AdminPage = () => {
         <RAdmin disableTelemetry dataProvider={ccDataProvider(ClassClockService.baseURL, getAccessTokenSilently)} authProvider={customAuthProvider} loginPage={LoginRedirect} title="ClassClock Admin">
           <Resource name="school" />
           <Resource name="bellschedule" options={{ label: 'Bell Schedules' }} list={BellScheduleList} edit={BellscheduleEdit} create={BellScheduleCreate} />
+          <CustomRoutes>
+            <Route path="/" element={
+              <Card>
+                <Title title="ClassClock Admin" />
+                <p>Welcome to the ClassClock Admin page.</p>
+                <p>Use the menu on the left to select what part of the app you would like to edit</p>
+                <br/>
+                <p>This admin page is designed to work on desktop devices.<br/>Use on a mobile device is not currently supported.</p>
+              </Card>
+            } />
+          </CustomRoutes>
+
         </RAdmin>
     </BrowserRouter>)
 }
